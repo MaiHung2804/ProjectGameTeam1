ff1407fe14f6f65672a3c66883108483347812dd
tqt20004 <thangtu0440@gmail.com>
2025-09-03 20:32:24 +0700

    Tạo ItemBase
    ItemBase
    -MeleeWeapon
    +baseKnife
    -RangedWeapon
    +arrow1
    -Consumable
    +health
    +mana

---
+++ Assets/Scripts/BaseItem/arrow1.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrow1 : RangedWeapon
+{
+    public override void Use(GameObject user)
+    {
+        ///logic attack 
+    }
+}
