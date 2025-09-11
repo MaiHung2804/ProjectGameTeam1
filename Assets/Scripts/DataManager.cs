e4cdf8043debef1f9d11669aeb049d92058bd7c8
Cheb131 <nguyengiatrieuvy@gmail.com>
2025-09-12 01:37:33 +0700

    Seventh Commit
    chưa hoàn thành save và load game.

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -1,60 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DataManager: MonoBehaviour
 {
-    public Player player;
+    public PlayerData player;
     void Start()
-    {   
-        if (player == null)
-        {
-            player = FindObjectOfType<Player>();
-        }
-        LoadData();
+    {
+        LoadPlayerName();
+        //LoadData();
+        //LoadPlayerName();
+        //LoadScore();
+        //LoadGold();
+        //LoadPosition();
+        //Debug.Log("Data Load Path: " + Application.persistentDataPath);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-     
-    }
+
 
     private void OnApplicationQuit()
     {
         SaveData();
     }
+
     public void SaveData()
     {
-        PlayerPrefs.SetInt("HighScore", player.HighScore);
-        PlayerPrefs.SetString("UserName", player.UserName);
-        PlayerPrefs.SetFloat("UserPositionX", player.UserPositions[0]);
-        PlayerPrefs.SetFloat("UserPositionY", player.UserPositions[1]);
-        PlayerPrefs.SetFloat("UserPositionZ", player.UserPositions[2]);
-        PlayerPrefs.SetInt("CurrentHp", player.CurrentHp);
-        PlayerPrefs.SetInt("CurrentLevel", player.CurrentLevel);
-        PlayerPrefs.SetInt("CurrentGold", player.CurrentGold);
+        PlayerPrefs.SetInt("HighScore", player.highScore);
+        PlayerPrefs.SetString("UserName", player.userName);
+        PlayerPrefs.SetFloat("UserPositionX", player.userPositions[0]);
+        PlayerPrefs.SetFloat("UserPositionY", player.userPositions[1]);
+        PlayerPrefs.SetFloat("UserPositionZ", player.userPositions[2]);
+        PlayerPrefs.SetInt("CurrentHp", player.currentHp);
+        PlayerPrefs.SetInt("CurrentLevel", player.currentLevel);
+        PlayerPrefs.SetInt("CurrentGold", player.currentGold);
         PlayerPrefs.Save();
         Debug.Log("Data Saved");
     }
-
-    public void LoadData()
+    public void DeleteData()
     {
-        player.HighScore = PlayerPrefs.GetInt("HighScore");
-        player.UserName = PlayerPrefs.GetString("UserName");
-        player.UserPositions[0] = PlayerPrefs.GetFloat("UserPositionX"); 
-        player.UserPositions[1] = PlayerPrefs.GetFloat("UserPositionY");
-        player.UserPositions[2] = PlayerPrefs.GetFloat("UserPositionZ");
-        player.CurrentHp = PlayerPrefs.GetInt("CurrentHp");
-        player.CurrentLevel = PlayerPrefs.GetInt("CurrentLevel");
-        player.CurrentGold = PlayerPrefs.GetInt("CurrentGold");
-        Debug.Log("UserName: " + player.UserName);
-        Debug.Log("HighScore: " + player.HighScore);
-        Debug.Log("UserPosition: " + player.UserPositions[0] + "," + player.UserPositions[1] + "," + player.UserPositions[2]);
-        Debug.Log("CurrentHp: " + player.CurrentHp);
-        Debug.Log("CurrentLevel: " + player.CurrentLevel);
-        Debug.Log("CurrentGold: " + player.CurrentGold);
-        Debug.Log("Data Load Pathh: " + Application.persistentDataPath);
+        PlayerPrefs.DeleteAll();
+        Debug.Log("Data Deleted");
+    }
 
+    public int LoadData()
+    {
+        player.currentHp = PlayerPrefs.GetInt("CurrentHp");
+        player.currentLevel = PlayerPrefs.GetInt("CurrentLevel");
+        Debug.Log("CurrentHp: " + player.currentHp);
+        Debug.Log("CurrentLevel: " + player.currentLevel);
+        Debug.Log("Data Load Pathh: " + Application.persistentDataPath);
+        return player.currentHp + player.currentLevel;
+    }
+    public string LoadPlayerName()
+    {
+        return player.userName = PlayerPrefs.GetString("UserName");    
+    }
+    public int LoadScore()
+    {
+        player.highScore = PlayerPrefs.GetInt("HighScore");
+        return player.highScore;
+        Debug.Log("HighScore: " + player.highScore);
+    }
+    public int LoadGold() 
+    {
+        player.currentGold = PlayerPrefs.GetInt("CurrentGold");
+        return player.currentGold;
+        Debug.Log("CurrentGold: " + player.currentGold);
+    }
+    public float LoadPosition()
+    {
+        player.userPositions[0] = PlayerPrefs.GetFloat("UserPositionX");
+        player.userPositions[1] = PlayerPrefs.GetFloat("UserPositionY");
+        player.userPositions[2] = PlayerPrefs.GetFloat("UserPositionZ");
+        return player.userPositions[0] + player.userPositions[1] + player.userPositions[2];
     }
 }
