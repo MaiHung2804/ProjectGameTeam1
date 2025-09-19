18b66a8a08203ae45f0355a14c83cc4d9be5a223
maihung2804jr <maihung2804jr@gmail.com>
2025-09-19 10:38:58 +0700

    no message

--- Assets/Scripts/Unit/Enemy/EnemyUnit.cs
+++ Assets/Scripts/Unit/Enemy/EnemyUnit.cs
@@ -2,17 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyUnit : MonoBehaviour
+public class EnemyUnit : UnitBase
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Enemy Settings")]
+    [SerializeField] private float detectionRange = 5f;
+    [SerializeField] private float attackRange = 1.5f;
+    [SerializeField] private float attackCooldown = 1.5f;
+
+    private Transform targetPlayer;
+    private float lastAttackTime = 0f;
+
+    private void Update()
     {
-        
+        if (IsDead) return;
+
+        if (targetPlayer == null)
+        {
+            FindPlayer();
+            return;
+        }
+
+        float distance = Vector3.Distance(transform.position, targetPlayer.position);
+
+        if (distance <= attackRange)
+        {
+            TryAttackPlayer();
+        }
+        else if (distance <= detectionRange)
+        {
+            moveComponent?.MoveTo(targetPlayer.position);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FindPlayer()
     {
-        
+        PlayerUnit player = FindObjectOfType<PlayerUnit>();
+        if (player != null)
+        {
+            targetPlayer = player.transform;
+        }
+    }
+
+    private void TryAttackPlayer()
+    {
+        if (Time.time - lastAttackTime < attackCooldown) return;
+
+        lastAttackTime = Time.time;
+
+        UnitBase playerUnit = targetPlayer.GetComponent<UnitBase>();
+        if (playerUnit != null)
+        {
+            attackComponent.Attack(playerUnit);
+        }
     }
 }
