162d38024a5e1277b6f0121f17d48e539713ef4e
maihung2804jr <maihung2804jr@gmail.com>
2025-09-19 10:32:17 +0700

    Merge remote-tracking branch 'origin/TrieuVy'
    # Conflicts:
    #	Assets/Scenes/Level1.unity
    #	Assets/Scripts/PlayerData.cs
    #	Assets/Scripts/Unit/Player/PlayerMoveComponent.cs

---
+++ Assets/Scripts/Config & Data/UIManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+public class UIManager : MonoBehaviour
+{
+    public bool isUIVisible = true;
+    public DataManager dataManager;
+    public GameObject CanvasMenu;
+    public GameObject CanvasInput;
+    public TMP_InputField playername;
+    public PlayerData playerData;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab)) // Nhấn Tab hiện UI Menu
+        {
+            isUIVisible = true;
+            Time.timeScale = 0f; // Tạm dừng game khi mở menu
+            CanvasMenu.SetActive(isUIVisible);
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape)) // Nhấn Esc ẩn UI Menu
+        {
+            isUIVisible = false;
+            Time.timeScale = 1f; // Tiếp tục game khi đóng menu
+            CanvasMenu.SetActive(isUIVisible);
+        }
+    }
+    public void NewGameButton() // Khi ấn vào nút Newgame, đặt các giá trị về ban đầu
+    {
+        dataManager.DeleteData();
+        ShowCanvasInput();        
+    } 
+    public void PlayButton(string sceneName) // Ấn vào Play load scene lv1
+    {
+        UserName();
+        SceneManager.LoadScene(sceneName);
+        Debug.Log("Play button clicked, loading scene: " + sceneName);
+
+    }
+    public void UserName()
+    {
+        string name = playername.text;
+        DataManager.Instance.player.userName = name;
+    }
+    public void ExitButton() // Ấn vào Exit thoát game
+    {
+        Application.Quit();
+    }
+    public void SaveData() // Ấn vào Save để lưu dữ liệu
+    {
+        DataManager.Instance.SaveData();        
+    }
+    public void ShowCanvasInput() // Hiện CanvasInput để nhập tên
+    {
+        CanvasMenu.SetActive(false);
+        CanvasInput.SetActive(true);
+    }
+    public void BackToMenu(string sceneName) // Quay về Menu chính
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void LoadGameButton() // Load dữ liệu khi ấn LoadGame
+    {
+        DataManager.Instance.LoadData();
+        int level = DataManager.Instance.player.userLevel;
+        string sceneName = "Level" + level;
+        SceneManager.LoadScene(sceneName);
+        Debug.Log("Load Game button clicked, loading scene: " + sceneName);
+
+    }
+
+}
---
+++ Assets/Scripts/Config & Data/UnitStat.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStat 
+{
+    public UnitConfig config;
+    public int level; // Cấp độ
+    public int currentExperience; // Kinh nghiệm hiện tại
+    public int maxExperience; // Kinh nghiệm tối đa để lên cấp
+    public int currentHealth; // Máu hiện tại
+    public int maxHealth; // Máu tối đa
+    public int attackBase; // Sát thương
+    public int attackSpeed; // Tốc độ đánh
+    public int defenseBase; // Phòng thủ
+    public float moveSpeed; // Tốc độ di chuyển
+    public int attackRange; // Tầm đánh
+    
+    public UnitStat(UnitConfig config)
+    {
+        this.config = config;
+        this.level = 1; // Mặc định cấp độ là 1
+        this.currentExperience = 0; // Mặc định kinh nghiệm hiện tại là 0
+        this.maxExperience = 100; // Mặc định kinh nghiệm tối đa là 100
+        this.maxHealth = 100; // Mặc định máu tối đa là 100
+        this.currentHealth = maxHealth; // Khởi tạo máu hiện tại bằng máu tối đa
+        this.attackBase = 10; // Mặc định sát thương cơ bản là 10
+        this.attackSpeed = 1; // Mặc định tốc độ đánh là 1
+        this.defenseBase = 5; // Mặc định phòng thủ cơ bản là 5
+        this.moveSpeed = 5; // Mặc định tốc độ di chuyển là 5
+        this.attackRange = config.type == EnemyType.Melee ? 1 : 5; // Tầm đánh dựa trên loại kẻ địch
+    }
+
+    public void GainExperience(int amount) // Nhận kinh nghiệm
+    {
+        currentExperience += amount;
+        if (currentExperience >= maxExperience)
+        {
+            LevelUp();
+        }
+    }
+    public void LevelUp() // Lên cấp
+    {
+        level++;
+        DamageUpg(level); // Nâng cấp sát thương
+        HealthUp(level); // Nâng cấp máu
+        DefenseUpg(level); // Nâng cấp phòng thủ
+    }
+    public void TakeDamage(int damage)
+    {
+        int damageTaken = damage - defenseBase;
+        if (damageTaken < 0)
+        {
+            damageTaken = 0;
+        }
+        currentHealth -= damageTaken;
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+            Die();
+        }
+    } // Nhận sát thương
+    public void Die()
+    {
+        
+        Debug.Log(config.enemyName + " has died.");
+    } // Chết
+    public void Heal(int amount) // Hồi máu
+    {
+        currentHealth += amount;
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+    public void DamageUpg(int upgradeAmount) // Nâng cấp sát thương
+    {
+        upgradeAmount = upgradeAmount * 5; // Mỗi lần nâng cấp tăng thêm 5 sát thương
+        attackBase += upgradeAmount;
+    }
+    public void HealthUp(int upgradeAmount) // Nâng cấp máu
+    {
+        upgradeAmount = upgradeAmount * 10; // Mỗi lần nâng cấp tăng thêm 10 máu
+        maxHealth += upgradeAmount;
+        currentHealth = maxHealth; // Hồi máu đầy khi nâng cấp
+        Debug.Log("Health upgraded by " + upgradeAmount + ". New HP: " + maxHealth);
+    }
+    public void DefenseUpg(int upgradeAmount) // Nâng cấp phòng thủ
+    {
+        upgradeAmount = upgradeAmount * 2; // Mỗi lần nâng cấp tăng thêm 2 phòng thủ
+        defenseBase += upgradeAmount;
+    }
+
+
+}
