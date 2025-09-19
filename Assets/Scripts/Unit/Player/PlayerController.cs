ce3ea422b321afc115647cf2eb1f202a05f4b108
Cheb131 <nguyengiatrieuvy@gmail.com>
2025-09-19 20:54:58 +0700

    Fix Conflict

--- Assets/Scripts/Unit/Player/PlayerController.cs
+++ Assets/Scripts/Unit/Player/PlayerController.cs
@@ -29,20 +29,20 @@ public class PlayerController : UnitController
     }
 
 
-    private bool GetDirectionFromDevices(out Vector3 direction, out float speedIntensity)
-    {
-        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) +
-                              new Vector3(joystick.Horizontal, 0, joystick.Vertical);
-        if (input.sqrMagnitude < inputVectorSqrMin)
-        {
-            direction = Vector3.zero;
-            speedIntensity = 0f;
-            return false;
-        }
-        speedIntensity = Mathf.Clamp01(input.magnitude);
-        direction = ConvertInputToDirectionByCamera(input);
-        return true;
-    }
+    //private bool GetDirectionFromDevices(out Vector3 direction, out float speedIntensity)
+    //{
+    //    Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) +
+    //    new Vector3(joystick.Horizontal, 0, joystick.Vertical);
+    //    if (input.sqrMagnitude < inputVectorSqrMin)
+    //    {
+    //        direction = Vector3.zero;
+    //        speedIntensity = 0f;
+    //        return false;
+    //    }
+    //    speedIntensity = Mathf.Clamp01(input.magnitude);
+    //    direction = ConvertInputToDirectionByCamera(input);
+    //    return true;
+    //}
 
     private Vector3 ConvertInputToDirectionByCamera(Vector3 input)
     {
